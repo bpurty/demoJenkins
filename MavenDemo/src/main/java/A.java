76313615b5fac0dc515f9f94f9787a102b0950a5
PURTY <bhanu.purty@capgemini.com>
2018-10-01 12:02:40 +0530

    maven added

---
+++ MavenDemo/src/main/java/A.java
@@ -0,0 +1,11 @@
+
+
+public class A extends B {
+
+	public static void main(String[] args) {
+		System.out.println("Hello jenkins");
+		B a=new B();
+		System.out.println(a.m);
+	}
+
+}
---
+++ MavenDemo/src/main/java/B.java
@@ -0,0 +1,7 @@
+
+public class B {
+	
+	
+     protected int m=10;
+ 
+}
